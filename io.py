ecdd77079874b929b24135870dca8b785e3a8ea7
srbhr <srbh077@gmail.com>
2021-07-30 16:34:36 +0530

    Improved IO file

--- io.py
+++ io.py
@@ -5,38 +5,22 @@ import io
 import os
 
 
-"""
-output as a list
+# output as a list
+#     arr = [1, 2, 3, 4]
+#   sys.stdout.write(" ".join(map(str, arr)) + "\n")
 
-arr = [1, 2, 3, 4]
-sys.stdout.write(" ".join(map(str, arr)) + "\n")
+# arr = [int(x) for x in stdin.readline().split()]
 
-"""
-
-
-"""
-using list comprehensions
-arr = [int(x) for x in stdin.readline().split()]
-"""
-
-# taking Input
-input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-
-# taking Input as a string
-number = input().decode()
-
-# Writing to the file
-sys.stdout.write(str(number[0]+number[-1])+'\n')
+def program():
+    return None
 
 
 def main():
     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
     testcases = int(input())
     for test in range(testcases):
-        # number = int.from_bytes(input(), "big")
-        number = input().decode()
-        number = list(map(int, str(number.strip())))
-        sys.stdout.write(str(number[0]+number[-1])+'\n')
+        number = int(input())
+        sys.stdout.write(str(program)+'\n')
 
 
 if __name__ == '__main__':
